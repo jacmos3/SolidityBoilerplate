cb4be4c7d20e263d352f76b8cba01167fb59ad85
jacmos3 <j.moscioni@semproxlab.it>
2021-09-27 02:02:19 +0200

    added routing and frontend layout

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,51 @@
 import React, {Component} from 'react';
+import Layout from '../components/Layout.js';
+import {Form, Button, Input, Message} from 'semantic-ui-react';
+import web3 from '../ethereum/web3';
+import {Router} from '../routes';
 import myContract from '../ethereum/myContract';
 
 class MyDapp extends Component{
   state = {
-    message:"Retrieving message from the blockchain"
+    message:'Retrieving message from the blockchain',
+    errorMessage:'',
+    loading:false,
+    newMessage:''
   };
 
   async componentDidMount(){
-    console.log("myContract follows");
-    console.log(myContract);
     const message = await myContract.methods.message().call();
     this.setState({message:message});
-    console.log(message);
+  }
+  onSubmit = async (event) => {
+    event.preventDefault();
+    this.setState({loading:true, errorMessage:''});
+    try{
+      const accounts = await web3.eth.getAccounts();
+      await myContract.methods.setMessage(this.state.newMessage).send({from:accounts[0]});
+      Router.reload(window.location.pathname)
+    }catch(err){
+      this.setState({errorMessage: err.message});
+    }
+    this.setState({loading:false})
   }
   render(){
-    return <h1>{this.state.message}</h1>
+    return (
+      <Layout>
+        <h1>{this.state.message}</h1>
+        <Form onSubmit = {this.onSubmit} error={!!this.state.errorMessage}>
+          <Form.Field>
+            <label>Change message</label>
+            <Input
+            value = {this.state.newMessage}
+             onChange = {event => this.setState({newMessage: event.target.value})}/>
+          </Form.Field>
+
+          <Message error header="Oops!" content = {this.state.errorMessage} />
+          <Button loading = {this.state.loading} primary>Change!</Button>
+        </Form>
+      </Layout>
+    )
   }
 }
 
